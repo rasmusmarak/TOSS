565575422bf96fb4d57966c144241b3020457f1d
rasmusmarak <rasmusmarak@gmail.com>
2023-02-28 00:00:18 +0100

    added pykep osculating orbital elements.

--- Introductory Tests/udp_initial_condition.py
+++ Introductory Tests/udp_initial_condition.py
@@ -5,8 +5,8 @@ from typing import Union
 # For computing trajectory
 from Trajectory import Trajectory
 
-# For choosing numerical integration method
-from Integrator import IntegrationScheme
+# For orbit representation (reference frame)
+import pykep as pk
 
 # Class representing UDP
 class udp_initial_condition:
@@ -18,11 +18,12 @@ class udp_initial_condition:
     boundaries for the state variables and computation of the fitness value for a given intial state. 
     """
 
-    def __init__(self, body_density, target_altitude, final_time, start_time, time_step, lower_bounds, upper_bounds, algorithm, radius_bounding_sphere):
+    def __init__(self, body_density, body_mu, target_altitude, final_time, start_time, time_step, lower_bounds, upper_bounds, algorithm, radius_bounding_sphere):
         """ Setup udp attributes.
 
         Args:
             body_density (float): Mass density of body of interest
+            body_mu (float): Gravitational parameter for celestial body.
             target_altitude (float): Target altitude for satellite trajectory. 
             final_time (float): Final time for integration.
             start_time (float): Start time for integration of trajectory (often zero)
@@ -39,18 +40,10 @@ class udp_initial_condition:
         # Assertions:
         assert target_altitude > 0
         assert all(np.greater(upper_bounds, lower_bounds))
-
-        # Assertions:
-        assert body_density > 0
-        assert target_altitude > 0
-        assert final_time > start_time
-        assert time_step <= (final_time - start_time)
-        assert lower_bounds.all() < upper_bounds.all()
-
-        # Setup equations of motion class
-        self.eq_of_motion = Equations_of_motion(self.mesh_vertices, self.mesh_faces, body_density)
+        assert body_mu > 0
 
         # Additional hyperparameters
+        self.body_mu = body_mu
         self.target_altitude = target_altitude     
         self.lower_bounds = lower_bounds
         self.upper_bounds = upper_bounds
@@ -64,6 +57,11 @@ class udp_initial_condition:
         Returns:
             fitness value (_float_): Difference between squared values of current and target altitude of satellite.
         """
+        
+        # Convert osculating orbital elements to cartesian for integration
+        r, v = pk.par2ic(E=x, mu=self.body_mu)
+        x = np.array(r.append(v))
+
         # Integrate trajectory
         _, squared_altitudes, collision_detected = self.trajectory.integrate(np.array(x))
 
