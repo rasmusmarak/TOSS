bb21b0d0f6bc37647663e5efe9064016c06a2d89
rasmusmarak <rasmusmarak@gmail.com>
2023-03-07 15:51:40 +0100

    Updated rotation test.

--- toss/tests/point_rotation_test.py
+++ toss/tests/point_rotation_test.py
@@ -6,18 +6,14 @@ sys.path.append("../..")
 
 # Import relevant modules
 import toss
-from toss import mesh_utility
-from toss.EquationsOfMotion import EquationsOfMotion
+from toss import equations_of_motion
 
 # Core packages
 from dotmap import DotMap
 import math
-from math import pi, radians
+from math import pi
 import numpy as np
 from numpy.random import randint, rand
-
-# For computing rotations of orbits
-from pyquaternion import Quaternion
 
 
 def test_rotation_of_point():
@@ -38,17 +34,14 @@ def test_rotation_of_point():
     """
 
     # Body parameters
-    body_args = DotMap()
-    body_args.density = 533                  # https://sci.esa.int/web/rosetta/-/14615-comet-67p
-    body_args.mu = 665.666                   # Gravitational parameter for 67P/C-G
-    body_args.declination = 64               # [degrees] https://sci.esa.int/web/rosetta/-/14615-comet-67p
-    body_args.right_ascension = 69           # [degrees] https://sci.esa.int/web/rosetta/-/14615-comet-67p
-    body_args.spin_period = 12.06*3600       # [seconds] https://sci.esa.int/web/rosetta/-/14615-comet-67p
-
-    # Creating the mesh (TetGen)
-    _, mesh_vertices, mesh_faces, _ = mesh_utility.create_mesh()
-    # Setup equations of motion class
-    eq_of_motion = EquationsOfMotion(mesh_vertices, mesh_faces, body_args)
+    args = DotMap()
+    args.body.density = 533                  # https://sci.esa.int/web/rosetta/-/14615-comet-67p
+    args.body.mu = 665.666                   # Gravitational parameter for 67P/C-G
+    args.body.declination = 64               # [degrees] https://sci.esa.int/web/rosetta/-/14615-comet-67p
+    args.body.right_ascension = 69           # [degrees] https://sci.esa.int/web/rosetta/-/14615-comet-67p
+    args.body.spin_period = 12.06*3600       # [seconds] https://sci.esa.int/web/rosetta/-/14615-comet-67p
+    args.body.spin_velocity = (2*pi)/args.body.spin_period
+    args.body.spin_axis = equations_of_motion.setup_spin_axis(args)
 
     # Define number of rotations to evaluate
     n_max = 10
@@ -64,23 +57,14 @@ def test_rotation_of_point():
 
         ######### Rotation using Quaternion #########
         # Rotation of point: 
-        rotated_position_quaternion = eq_of_motion.rotate_point(t, x)
-        #############################################
+        rotated_position_quaternion = equations_of_motion.rotate_point(t, x, args)
 
         #########    Analtical rotation    #########
-        # Define spin axis as in EquationsOfMotion:
-        spin_velocity = (2*pi)/body_args.spin_period
-        q_dec = Quaternion(axis=[1,0,0], angle=radians(body_args.declination)) # Rotate spin axis according to declination
-        q_ra = Quaternion(axis=[0,0,1], angle=radians(body_args.right_ascension)) # Rotate spin axis accordining to right ascension
-        q_axis = q_dec * q_ra  # Composite rotation of q1 then q2 expressed as standard multiplication
-        spin_axis = q_axis.rotate([0,0,1])
-
-
         # Define analytical rotation (euler-rodrigues):
-        axis = np.asarray(spin_axis)
+        axis = np.asarray(args.body.spin_axis)
         axis = axis / math.sqrt(np.dot(axis, axis))
-        a = math.cos((2*pi - (spin_velocity*t))/2.0)
-        b, c, d = -axis * math.sin((2*pi - (spin_velocity*t))/ 2.0)
+        a = math.cos((2*pi - (args.body.spin_velocity*t))/2.0)
+        b, c, d = -axis * math.sin((2*pi - (args.body.spin_velocity*t))/ 2.0)
         aa, bb, cc, dd = a * a, b * b, c * c, d * d
         bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
 
