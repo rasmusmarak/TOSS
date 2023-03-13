bf391cfa901151987e292f507f4dc9b25066f52a
rasmusmarak <rasmusmarak@gmail.com>
2023-03-13 11:32:59 +0100

    test.

--- toss/trajectory_tools.py
+++ toss/trajectory_tools.py
@@ -72,7 +72,7 @@ def compute_trajectory(x: np.ndarray, args, func: Callable) -> Union[np.ndarray,
         time_list = [args.problem.start_time, args.state.time_of_maneuver, args.problem.final_time]
 
     # Integrate trajectory for each subinterval
-    #trajectory_info = 0
+    trajectory_info = 0
     for i in range(0, len(time_list)-1):
         args.integrator.t0 = time_list[i]
         args.integrator.tf = time_list[i+1]
