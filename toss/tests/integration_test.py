7354a47b270c88ca4144c3772f5251f5bb6c0c74
Rasmus Maråk <marak@kth.se>
2023-04-06 00:14:32 +0200

    updated import.

--- toss/tests/integration_test.py
+++ toss/tests/integration_test.py
@@ -1,9 +1,9 @@
 """ This test checks whether or not the integration is performed correctly """
 
 # Import required modules
-from ..equations_of_motion import setup_spin_axis, compute_motion
-from ..mesh_utility import create_mesh
-from ..trajectory_tools import compute_trajectory
+from equations_of_motion import setup_spin_axis, compute_motion
+from mesh_utility import create_mesh
+from trajectory_tools import compute_trajectory
 
 # Core packages
 from dotmap import DotMap
