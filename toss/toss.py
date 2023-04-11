9174be7dfe2e314516230ff64dce7191a00571aa
rasmusmarak <rasmusmarak@gmail.com>
2023-04-11 16:19:59 +0200

    .

--- toss/toss.py
+++ toss/toss.py
@@ -80,13 +80,15 @@ def main():
 
 
 if __name__ == "__main__":
-    cProfile.run("main()", "output.dat")
+        
+    main()
+    #cProfile.run("main()", "output.dat")
 
-    with open("output_time.txt", "w") as f:
-        p = pstats.Stats("output.dat", stream=f)
-        p.sort_stats("time").print_stats()
+    #with open("output_time.txt", "w") as f:
+    #    p = pstats.Stats("output.dat", stream=f)
+    #    p.sort_stats("time").print_stats()
     
-    with open("output_calls.txt", "w") as f:
-        p = pstats.Stats("output.dat", stream=f)
-        p.sort_stats("calls").print_stats()
+    #with open("output_calls.txt", "w") as f:
+    #    p = pstats.Stats("output.dat", stream=f)
+    #    p.sort_stats("calls").print_stats()
         
