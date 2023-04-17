e4eecf817b73f4d6a95e54c15650f73c589021cd
Rasmus Maråk <marak@kth.se>
2023-04-17 15:55:01 +0200

    Computing trajectory for each spacecraft.

--- toss/optimization/udp_initial_condition.py
+++ toss/optimization/udp_initial_condition.py
@@ -47,7 +47,7 @@ class udp_initial_condition:
                     radius_outer_bounding_sphere (float): Radius of the outer bounding sphere.
                     squared_volume_inner_bounding_sphere (float): squared volume of the inner bounding sphere.
                     squared_volume_outer_bounding_sphere (float): squared volume of the outer bounding sphere.
-                    measurable_squared_volume (float): Total measurable volume, i.e:    V_outer_sphere - V_inner_sphere.
+                    total_measurable_volume (float): Total measurable volume, i.e:    V_outer_sphere - V_inner_sphere.
                     measurement_period (int): Period for which a measurment sphere is recognized and managed.
                 mesh:
                     vertices (np.ndarray): Array containing all points on mesh.
@@ -66,7 +66,7 @@ class udp_initial_condition:
         r_outer_sphere = args.problem.radius_outer_bounding_sphere
         v_inner_sphere =  args.problem.squared_volume_inner_bounding_sphere
         v_outer_sphere =  args.problem.squared_volume_outer_bounding_sphere
-        v_measurable = args.problem.measurable_squared_volume
+        v_measurable = args.problem.total_measurable_volume
         largest_protuberant = args.mesh.largest_body_protuberant
         density = args.body.density
         dec = args.body.declination
@@ -75,7 +75,6 @@ class udp_initial_condition:
         n_maneuvers = args.problem.number_of_maneuvers
         activate_events = args.problem.activate_event
         meaurement_period = args.problem.measurement_period
-        fitness_list = args.problem.selected_fitness_functions
 
         # Assertions:
         assert self.target_sq_alt > 0
@@ -95,8 +94,6 @@ class udp_initial_condition:
         assert (v_outer_sphere > v_inner_sphere)
         assert (v_measurable > 0)
         assert (meaurement_period > 0)
-        assert (len(fitness_list) > 0)
-
 
         # Additional hyperparameters
         self.args = args
@@ -114,20 +111,28 @@ class udp_initial_condition:
             fitness (_float_): Evaluated fitness for user-specified fitness-function.
         """
 
-        # Compute trajectory
-        collision_detected, list_of_ode_objects, _ = compute_trajectory(x, self.args, compute_motion)
+        # Separate each chromosome representing an induvidual spacecraft:
+        list_of_spacecrafts = np.array_split(x, self.args.problem.number_of_spacecrafts)
 
-        # If collision detected => unfeasible trajectory
-        if collision_detected:
-            fitness = 1e30
-            return [fitness]
+        # Compute fitness of each spacecraft individually:
+        fitness = 0
+        for spacecraft in list_of_spacecrafts:
         
-        # Get positions on trajectory for a fixed time-step
-        positions, _, timesteps = get_trajectory_fixed_step(self.args, list_of_ode_objects)
+            # Compute trajectory
+            collision_detected, list_of_ode_objects, _ = compute_trajectory(spacecraft, self.args, compute_motion)
 
-        # Compute fitness:
-        chosen_fitness_function = FitnessFunctions.CoveredVolumeFarDistancePenalty
-        fitness = get_fitness(chosen_fitness_function, self.args, positions, timesteps)
+            # If collision detected => unfeasible trajectory
+            if collision_detected:
+                fitness = 1e30
+                return [fitness]
+            
+            # Get positions on trajectory for a fixed time-step
+            positions, _, timesteps = get_trajectory_fixed_step(self.args, list_of_ode_objects)
+
+            # Compute fitness:
+            chosen_fitness_function = FitnessFunctions.CoveredVolumeFarDistancePenalty
+            fitness += get_fitness(chosen_fitness_function, self.args, positions, timesteps)
+
         return [fitness]
 
 
