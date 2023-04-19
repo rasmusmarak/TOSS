a71b6c8cb1a69eff3de5723b05913bd823ee9246
rasmusmarak <rasmusmarak@gmail.com>
2023-04-19 16:30:44 +0200

    adjusted init.py

--- toss/__init__.py
+++ toss/__init__.py
@@ -1,10 +1,10 @@
-from equations_of_motion import setup_spin_axis, compute_motion, rotate_point
-from mesh_utility import create_mesh, is_outside
-from trajectory_tools import compute_trajectory
-from .utilities.load_default_cfg import load_default_cfg
-from udp_initial_condition import udp_initial_condition
-from setup_parameters import setup_parameters
-from Integrator import IntegrationScheme
+from toss.equations_of_motion import setup_spin_axis, compute_motion, rotate_point
+from toss.mesh_utility import create_mesh, is_outside
+from toss.trajectory_tools import compute_trajectory
+from toss.utilities.load_default_cfg import load_default_cfg
+from toss.udp_initial_condition import udp_initial_condition
+from toss.setup_parameters import setup_parameters
+from toss.Integrator import IntegrationScheme
 
 __all__ = [
     "setup_spin_axis",
