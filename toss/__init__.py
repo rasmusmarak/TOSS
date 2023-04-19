03d57120d29d821055763e871e037ae8b311c6d8
rasmusmarak <rasmusmarak@gmail.com>
2023-04-19 16:22:05 +0200

    Adjusted paths in init file for toss.

--- toss/__init__.py
+++ toss/__init__.py
@@ -1,10 +1,10 @@
-from .equations_of_motion import setup_spin_axis, compute_motion, rotate_point
-from .mesh_utility import create_mesh, is_outside
-from .trajectory_tools import compute_trajectory
+from equations_of_motion import setup_spin_axis, compute_motion, rotate_point
+from mesh_utility import create_mesh, is_outside
+from trajectory_tools import compute_trajectory
 from .utilities.load_default_cfg import load_default_cfg
-from .udp_initial_condition import udp_initial_condition
-from .setup_parameters import setup_parameters
-from .Integrator import IntegrationScheme
+from udp_initial_condition import udp_initial_condition
+from setup_parameters import setup_parameters
+from Integrator import IntegrationScheme
 
 __all__ = [
     "setup_spin_axis",
