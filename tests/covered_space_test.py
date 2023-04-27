34865eef5ec911d8e269660127897399034e0c5c
rasmusmarak <rasmusmarak@gmail.com>
2023-04-27 13:46:43 +0200

    updated

--- tests/covered_space_test.py
+++ tests/covered_space_test.py
@@ -44,13 +44,12 @@ def test_perfect_ratio():
     radius_max = 8
 
     # Fixed maximal velocity from previously defined trajectory. 
-    scaling_factor = 2
-    velocities = np.array([-0.02826052, 0.1784372, -0.29885126]) * scaling_factor
-    
-    # Define frequency of points on grid:
-    #max_velocity = np.sqrt(np.max(velocities[0,:]**2 + velocities[1,:]**2 + velocities[2,:]**2))
-    max_velocity = np.max(np.linalg.norm(velocities))
-    time_step = timesteps[1] - timesteps[0]
+    scaling_factor = 1
+    fixed_velocity = np.array([-0.02826052, 0.1784372, -0.29885126]) * scaling_factor
+
+    # Define frequency of points for the spherical meshgrid: (see: Courant–Friedrichs–Lewy condition)
+    max_velocity = np.max(np.linalg.norm(fixed_velocity))
+    time_step = timesteps[1]-timesteps[0]
     max_distance_traveled = max_velocity * time_step
 
     # Calculate and adjust grid spacing based on maximal velocity and time step
@@ -61,7 +60,7 @@ def test_perfect_ratio():
     r = np.linspace(radius_min, radius_max, int(r_steps)) # Number of evenly spaced points along the radial axis
     theta = np.linspace(-np.pi/2, np.pi/2, int(theta_steps)) # Number of evenly spaced points along the polar angle/elevation (defined on [-pi/2, pi/2])
     phi = np.linspace(-np.pi, np.pi, int(phi_steps)) # Number of evenly spaced points along the azimuthal angle (defined on [-pi, pi])
-    
+
     # Generate spherical coordinates for every point on the grid
     array_of_spherical_coordinates = np.empty((3, int(1e8)), dtype=np.float64)
     idx = 0
@@ -75,8 +74,7 @@ def test_perfect_ratio():
 
     # Evaluate the ration of visited points, where the positions are every point on the corresponding grid.
     x_pos, y_pos, z_pos = sp2cart(array_of_spherical_coordinates[0,:], array_of_spherical_coordinates[1,:], array_of_spherical_coordinates[2,:])
-    noise = 1e-2
-    positions = np.vstack((x_pos,y_pos,z_pos)) - noise
-    ratio = compute_space_coverage(positions, velocities, timesteps, radius_min, radius_max)
+    positions = np.vstack((x_pos,y_pos,z_pos))
+    ratio = compute_space_coverage(positions, fixed_velocity, timesteps, radius_min, radius_max)
 
     assert (ratio==float(1))
