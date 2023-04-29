35510b74ffe36e6c868eaf06e0973594754b0fc6
rasmusmarak <rasmusmarak@gmail.com>
2023-04-29 18:25:37 +0200

    removed absolute paths imports (using init file).

--- tests/covered_space_test.py
+++ tests/covered_space_test.py
@@ -18,8 +18,6 @@ def test_large_random_sample():
     # Generate random sample of points defined on [radius_min, radius_max]
     positions = (radius_max)*np.random.random_sample((3,number_of_samples)) + (radius_min/2)
 
-    print(positions)
-
     # Generate random sample of velocities defined on [-1, 1]
     velocities = 2*np.random.random_sample((3,number_of_samples)) - 1
 
