5e141b9e12844f27fec218f557c57edc640baa8c
rasmusmarak <rasmusmarak@gmail.com>
2023-05-29 16:22:17 +0200

    added 2d plot.

--- toss/__init__.py
+++ toss/__init__.py
@@ -7,7 +7,7 @@ from .trajectory.trajectory_tools import get_trajectory_fixed_step, get_trajecto
 from .fitness.fitness_functions import target_altitude_distance, close_distance_penalty, far_distance_penalty, covered_volume, total_covered_volume, covered_space
 from .mesh.mesh_utility import create_mesh, is_outside
 from .trajectory.Integrator import IntegrationScheme
-from .visualization.plotting_utility import plot_UDP, fitness_over_generations, fitness_over_time, distance_deviation_over_time
+from .visualization.plotting_utility import plot_UDP_3D, plot_UDP_2D, fitness_over_generations, fitness_over_time, distance_deviation_over_time
 from .utilities.load_default_cfg import load_default_cfg
 from .optimization.udp_initial_condition import udp_initial_condition
 from .optimization.setup_parameters import setup_parameters
@@ -36,7 +36,8 @@ __all__ = [
     "create_mesh",
     "is_outside",
     "IntegrationScheme",
-    "plot_UDP",
+    "plot_UDP_3D",
+    "plot_UDP_2D",
     "fitness_over_generations",
     "fitness_over_time",
     "distance_deviation_over_time",
